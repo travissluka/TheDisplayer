18686f9a25ad889f55257d254eb1c598fbb8475d
Cory Martin <cmartin@atmos.umd.edu>
2015-06-16 22:11:12 -0400

    Added more sample plugins
    Metograds, AMS, etc

--- plugins/support/nwsTest.py
+++ plugins/support/nwsTest.py
@@ -11,6 +11,7 @@ if status == 1:
             #feed = 'http://alerts.weather.gov/cap/wwacapget.php?x=TX1253A8F521D0.FlashFloodWarning.1253A9013088TX.FWDFFWFWD.989125f0c5d821dcbc26f91893061aed'
             out = nwsfeed.ParseFeed(str(feed))
             alertdict = nwsfeed.AlertInfo(out)
+            print alertdict['expires']
             print alertdict['type']
             print alertdict['short']
             print alertdict['detail']
