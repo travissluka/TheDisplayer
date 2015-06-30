286a6d9d2b5a764f5c73ced64237065df7780df3
Travis Sluka <tsluka@atmos.umd.edu>
2015-06-30 14:10:42 -0400

    wrapped up the 'default' plugin

--- plugins/default/__init__.py
+++ plugins/default/__init__.py
@@ -1,19 +1,23 @@
 # Display the default fallback displays for when there is nothing
-# else to show
+# else to show, consisting of just the AOSC logo and a clock
+################################################################################
 import datetime as dt
 import os
 
 htmlpfx = 'file://'+os.path.abspath(os.path.dirname(__file__))
+lowPriorityParams = {
+    'enabled'     : True,
+    'updateFreq'  : dt.timedelta(hours=24),
+    'dispDuration': dt.timedelta(hours=1),
+    'priority'    : (0,1.0)
+}
 
 class Header:
     def getParams(self):
-        params = {}
-        params['enabled']       = True
-        params['updateFreq']    = dt.timedelta(seconds=60)
-        params['dispDuration']  = dt.timedelta(seconds=60)
-        params['priority']      = (0,1.0)
+        params = lowPriorityParams
         params['location']      = 'header'
         return params
+
     def getPage(self):
         html = htmlpfx+'/header.html'
         return html
@@ -22,17 +26,29 @@ class Header:
 class Footer:
     def getParams(self):
         params = {}
-        params['enabled']       = True
-        params['updateFreq']    = dt.timedelta(seconds=60)
-        params['dispDuration']  = dt.timedelta(seconds=60)
-        params['priority']      = (0,1.0)
+        params = lowPriorityParams
         params['location']      = 'footer'
         return params
+
     def getPage(self):
         html = htmlpfx+'/footer.html'
+        return html
+
+# The half class will be added to the list twice, 
+#  so that it can show up in both halves if there is 
+#  absolutely nothing to display (should never happen though, hopefully)
+class Half:
+    def getParams(self):
+        params = {}
+        params = lowPriorityParams
+        params['location']      = 'half'
+        return params
+
+    def getPage(self):
+        html = htmlpfx+'/half.html'
         return html
 
 
 ## the list of all available displays in this plugin,
 ## as required by the plugin loader
-displays = [Header, Footer]
+displays = [Header, Footer, Half, Half]
